31db8e634223b2de4909ca7c7ee16ab1a96631a1
Jaycom17 <juanorejuela499@gmail.com>
2023-11-18 18:06:45 -0500

    test with a new request method

--- src/routes/labors.routes.ts
+++ src/routes/labors.routes.ts
@@ -7,21 +7,16 @@ import { authCordinator } from "../middlewares/auth.middleware";
 const laborRouter = Router();
 
 
-<<<<<<< HEAD
 laborRouter.post("/labor", authCordinator, createLabor); //Create
-=======
-laborRouter.get("/labor/:name", showLaborByName); //GetByName
 
-laborRouter.post("/labor",authCordinator, createLabor); //Create
->>>>>>> d1efe425fccca582e5ea831abaac5c5961074ba6
+laborRouter.get("/labor/:name", authCordinator, showLaborByName); //GetByName
 
-laborRouter.get("/labor/:id", authCordinator, showLaborByID); //GetByID
+laborRouter.post("/labor",authCordinator, createLabor); //Create 
 
 laborRouter.get("/labor", authCordinator, showLaborList) //GetAll
 
 laborRouter.put("/labor", authCordinator, updateLabor); //Update
 
 laborRouter.delete("/labor/:id", authCordinator, deleteLabor); //Delete
-
 
 export default laborRouter;
